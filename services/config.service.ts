2d22b58a2003339534119d9a3d32945108c50cdd
Elton Marku <eltonmarku@gmail.com>
2020-11-01 21:17:31 +0100

    .

--- services/config.service.ts
+++ services/config.service.ts
@@ -3,11 +3,23 @@ import {config} from 'https://deno.land/x/dotenv/mod.ts';
 // deno eval "console.log(Deno.env.toObject())"
 
 export class ConfigService {
-    static getConfigKey(key: string): string {
+    static getConfigKey(key: any): string {
         if (config()[key]) {
             return config()[key];
         } else if (Deno.env.get(key)) {
             return Deno.env.get(key)!;
+        } else if (ConfigService.getDenoArg(key)) {
+            return ConfigService.getDenoArg(key);
+        }
+        return '';
+    }
+
+    static getDenoArg(key: string) {
+        for(var i = 0; i < Deno.args.length; i++) {
+            const keyValuePair = Deno.args[i].split('=');
+            if (keyValuePair[0] === key) {
+                return keyValuePair[1];
+            }
         }
         return '';
     }
